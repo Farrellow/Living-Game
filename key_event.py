b3a8283c06d9bd1fa6b1c5f388af109e346883d3
Farrell <farrellow@gmail.com>
2017-06-06 10:17:40 +0800

    dev: complete keyboard input module

--- key_event.py
+++ key_event.py
@@ -3,38 +3,64 @@
 import evdev
 import os
 import select
-import sys
+import struct
 
-def detectInputKey():
-    dev = evdev.InputDevice('/dev/input/event0')
-    while True:
-        select.select([dev], [], [])
-        for event in dev.read():
-            if (event.value == 1 or event.value == 0) and event.code != 0:
-                print("Key: %s Status: %s" % (event.code, "pressed" if event.value else "release"))
+class KeyboardEventListen(object):
+    def __init__(self):
+        self.file_fd = {}
+        self.epoll = select.epoll()
+        self.s_set = set([
+            35,  # h
+            36,  # j
+            37,  # k
+            38,  # l
+            28,  # Enter
+            ])
+        self.prev_code = 0
 
-def haha():
-    print(os.getpid())
-    #dev = evdev.InputDevice('/dev/input/event10')
-    dev = evdev.InputDevice('/dev/input/by-path/pci-0000:00:1d.0-usb-0:1.5:1.2-event-kbd')
-    epoll = select.epoll()
-    epoll.register(dev.fileno(), select.EPOLLIN)
-    try:
-        while True:
-            events = epoll.poll(1)
-            for fileno, event in events:
-                if event & select.EPOLLIN:
-                    for e in dev.read():
-                        if (e.value == 1):
-                            print('e = %s, code = %s' % (e, e.code))
-    finally:
-        epoll.unregister(dev.fileno())
-        epoll.close()
+        in_device = os.listdir('/dev/input/by-path')
+        for device in in_device:
+            if 'kbd' in device:
+                path = os.path.join('/dev/input/by-path', device)
+                dev = evdev.InputDevice(path)
+                self.file_fd[dev.fileno()] = dev
+                self.epoll.register(dev.fileno(), select.EPOLLIN)
+
+    def listening(self):
+        with open('kbd_fifo', 'wb') as f:
+            try:
+                while True:
+                    events = self.epoll.poll(1)
+                    for fileno, event in events:
+                        if event & select.EPOLLIN:
+                            for e in self.file_fd[fileno].read():
+                                if (e.value == 1):
+                                    self.event_handler(f, e.code)
+            finally:
+                for fileno, dev in self.file_fd.items():
+                    self.epoll.unregister(fileno)
+                self.epoll.close()
+
+    def event_handler(self, f, code):
+        if code in self.s_set:
+            b = struct.pack('>B', code)
+            f.write(b)
+            f.flush()
+            self.last_code = code
+        else:
+            if code == 0:
+                if self.last_code in self.s_set:
+                    b = struct.pack('>B', self.last_code)
+                    f.write(b)
+                    f.flush()
+                else:
+                    pass
+            else:
+                self.last_code = code
 
 if __name__ == '__main__':
-    ##detectInputKey()
-    print(os.getpid())
-    if os.geteuid():
-        args = [sys.executable] + sys.argv
-        os.execlp('sudo', 'sudo', *args)
-    haha()
+    # 1.init keyboard event listen
+    # 2.open fifo file with write mode
+    # 3.send data to parent process when keyboard event happen
+    keyboard_event = KeyboardEventListen()
+    keyboard_event.listening()
--- main.py
+++ main.py
@@ -1,17 +1,76 @@
 # -*- coding: utf-8 -*-
 
+import os
+import random
+import socket
+import struct
 import sys
+import time
 
-from init import initialize, randan_init
-from logic import next_step, show_display, _print
+from display import Window
+
+def maingg():
+    port = 10000
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('127.0.0.1', port))
+        sock.listen(4)
+        client, address = sock.accept()
+        while True:
+            b = client.recv(1)
+            i = struct.unpack('>B', b)[0]
+            if i == 35:    # h
+                print(i)
+            elif i == 36:  # j
+                print(i)
+            elif i == 37:  # k
+                print(i)
+            elif i == 38:  # l
+                print(i)
+            elif i == 28:  # Enter
+                print(i)
+            elif i == 0:   # continuous
+                print(i)
+            else:          # others
+                pass
+    except KeyboardInterrupt as e:
+        print('KeyboardInterrupt: %s' % e)
+    except Exception as e:
+        print('Exception: %s' % e)
+    finally:
+        client.close()
+        sock.close()
+
+def create_fifofile(path):
+    try:
+        os.mkfifo(path, 0o777)
+    except:
+        pass
+
+def main_process():
+    # 1.display init
+    # 2.open fifo file with read mode
+    # 3.while True: handler receive to change display
+    with open('kbd_fifo', 'rb') as f:
+        while True:
+            b = f.read(1)
+            if b:
+                code = struct.unpack('>B', b)[0]
+                print('code = %s' % code)
+            else:
+                break
+
+def main():
+    create_fifofile('kbd_fifo')
+    pid = os.fork()
+    if pid > 0:
+        # parent process
+        main_process()
+    elif pid == 0:
+        # child process
+        os.execlp('sudo', 'sudo', 'python3', 'key_event.py')
+    else:
+        print('error')
 
 if __name__ == '__main__':
-    #initialize()
-    randan_init()
-    i = 0
-    while True:
-        input()
-        next_step()
-        show_display()
-        i += 1
-        _print(0, 0, str(i))
+    main()
